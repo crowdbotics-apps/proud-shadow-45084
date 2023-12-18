e43aa83c33bc6e60cc60a1be12a8d946174ad9af
Crowdbotics <team@crowdbotics.com>
2023-12-18 20:05:41 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "proud-shadow-45084",
+  url: "https://proud-shadow-45084.botics.co",
+  api: "https://proud-shadow-45084.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
